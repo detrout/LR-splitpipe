9b66bdeb68f1b2103f1599a6389703111a7c5521
Diane Trout <diane@caltech.edu>
2023-11-09 17:05:02 -0800

    Test that the main function can run successfully.
    This required adding a small fastq file, and removing the pattern in
    .gitignore to ignore fastq files.
    
    These tests are pretty minimal in that they just call some common
    LR-splitpipe commands and make sure it doesn't throw an exception.
    
    Better tests would actually check the output files.

--- testing/test_demultiplex.py
+++ testing/test_demultiplex.py
@@ -1,22 +1,68 @@
-import pytest
-# from pacbio-splitpipe.demultiplex import *
-import sys
-sys.path.append('../pacbio-splitpipe')
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from unittest import TestCase
 
-# tests the functions in superclass Graph.py
-class TestBoi(object):
-	pass
+from LRsplitpipe.demultiplex import main, get_args
 
-
-# 	# tests get_transcript_min_max
-# 	def test_get_transcript_min_max(self):
-# 		a = get_dummy_sg()
-# 		assert a.get_transcript_min_max(0) == (1,3)
-# 		assert a.get_transcript_min_max(1) == (2,3)
-# 		assert a.get_transcript_min_max(2) == (1,2)
-
-def get_test_data():
+testing = Path(__file__).parent
 
 
+class TestDemultiplexMain(TestCase):
+    def test_running_score_linkers_t1(self):
+        fq = testing / "fq1.fastq"
+        with TemporaryDirectory("lrsplit") as tmpdir:
+            prefix = Path(tmpdir) / "score_linkers_tq"
+            main(["score_linkers", "-k", "WT", "-f", str(fq), "-o", str(prefix)])
 
+    def test_running_score_linkers_t2(self):
+        fq = testing / "fq1.fastq"
+        with TemporaryDirectory("lrsplit") as tmpdir:
+            prefix = Path(tmpdir) / "score_linkers_t2"
+            main(
+                [
+                    "score_linkers",
+                    "-t",
+                    "2",
+                    "-k",
+                    "WT",
+                    "-f",
+                    str(fq),
+                    "-o",
+                    str(prefix),
+                ]
+            )
 
+    def test_running_all_t1(self):
+        fq = testing / "fq1.fastq"
+        with TemporaryDirectory("lrsplit") as tmpdir:
+            prefix = Path(tmpdir) / "all_t1"
+            main(["all", "-k", "WT", "-f", str(fq), "-o", str(prefix)])
+
+    def test_running_all_t2(self):
+        fq = testing / "fq1.fastq"
+        with TemporaryDirectory("lrsplit") as tmpdir:
+            prefix = Path(tmpdir) / "all_t2"
+            main(["all", "-t", "2", "-k", "WT", "-f", str(fq), "-o", str(prefix)])
+
+    def test_parse_args(self):
+        args = get_args(
+            [
+                "score_linkers",
+                "-t",
+                "2",
+                "-k",
+                "WT",
+                "-f",
+                "name.fastq.gz",
+                "-o",
+                "target/name",
+            ]
+        )
+        self.assertEqual(args.mode, "score_linkers")
+        self.assertEqual(args.threads, "2")
+        self.assertEqual(args.kit, "WT")
+
+    def test_version(self):
+        from LRsplitpipe import _version
+
+        self.assertEqual(len(_version.version_tuple), 4)
